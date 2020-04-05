8f794e9c2cdc2a05b5d144e1fd3baa6ea220cffa
nicohirsau <preussens.nico@gmail.com>
2020-04-05 14:08:22 +0200

    first implementation of canvas class

---
+++ include/Mantaray/GLObjects/GLCanvas.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Mantaray/Core/Vector.hpp"
+#include "Mantaray/Core/Color.hpp"
+#include "Mantaray/Core/Shape.hpp"
+
+namespace MR {
+class GLCanvas {
+    public:
+        GLCanvas(Vector2u resolution, Vector2f coordinateScale);
+        GLCanvas(Vector2u resolution);
+        ~GLCanvas();
+
+        void bind();
+        void unbind();
+        void clear(Color color = Color(0x00u));
+        void display(Rectanglei viewPort);
+        void display(Rectanglei viewPort, Rectanglef destination);
+
+        void draw(
+            class Texture* texture, 
+            Vector2f position = Vector2f(0, 0), 
+            Vector2f size = Vector2f(1, 1),
+            bool absoluteSize = true,
+            float rotation = 0,
+            Vector2f rotationCenter = Vector2f(0, 0),
+            Rectanglef sourceRectangle = Rectanglef(0, 0, 1, 1),
+            Color color = Color(0xFFu),
+            class Shader* shader = nullptr
+        );
+
+    private:
+        void init(Vector2u resolution, Vector2f coordinateScale);
+
+    private:
+        class RenderTexture* m_RenderTexture = nullptr;
+        class Shader* m_Shader = nullptr;
+        Vector2f m_CoordinateScale = Vector2f(1, 1);
+        
+        static class VertexArray* DefaultVertexArray;
+        static class Shader* DefaultShader;
+};
+}
